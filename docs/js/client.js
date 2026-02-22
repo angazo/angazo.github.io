4b80bf09a88c1a10cfc9996f1db1975e02f20712
Juan Franco López <juan.franco.lopez@hotmail.com>
2026-02-22 17:44:25 +0100

    Se añade el efecto estrellas en la pantalla inicial.

--- docs/js/client.js
+++ docs/js/client.js
@@ -1,21 +1,13 @@
-// Script para mostrar la fecha actual en el div con id 'fecha'
 window.addEventListener('DOMContentLoaded', function() {
-
-
-
   const fechaDiv = document.getElementById('date');
   function actualizarFechaHora() {
     const ahora = new Date();
-    // Formato: día/mes/año hora:minuto:segundo
     const fechaHora = ahora.toLocaleDateString() + ' ' +
       ahora.toLocaleTimeString();
     fechaDiv.textContent = fechaHora;
   }
   actualizarFechaHora();
-
-  
   setInterval(() => {
     actualizarFechaHora();
   }, 100);
-
 });
