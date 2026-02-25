befa4efb69f54854ef999d29672384e7f8b646e8
Juan Franco López <juan.franco.lopez@hotmail.com>
2026-02-25 20:18:08 +0100

    Creación de la pantalla del juego en negro con un simple botón para salir que se maximiza en un móvil.

--- docs/js/game/scene/StartScene.js
+++ docs/js/game/scene/StartScene.js
@@ -1,31 +1,39 @@
 class StartScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor('#000000');
-    const isMobile = this.sys.game.device.os.android || this.sys.game.device.os.iOS;
-    if (isMobile) {
-      this._lockLandscape();
-    }
+    this._isMobile = this.sys.game.device.os.android || this.sys.game.device.os.iOS;
     this._createCloseButton();
     this.scale.on('resize', (gameSize) => {
       this._repositionCloseButton(gameSize.width);
     });
-    if (isMobile) {
-      this.time.delayedCall(300, () => {
-        if (!this.scale.isFullscreen) {
-          this.scale.startFullscreen();
-        }
-      });
+    if (this._isMobile) {
+      this.input.once('pointerdown', () => this._enterFullscreenLandscape());
     }
   }
-  _lockLandscape() {
-    try {
-      screen.orientation.lock('landscape').catch(() => {});
-    } catch (e) {}
+  _enterFullscreenLandscape() {
+    document.addEventListener('fullscreenchange', () => {
+      if (document.fullscreenElement) {
+        try {
+          screen.orientation.lock('landscape').catch(() => {});
+        } catch (e) {}
+      }
+    }, { once: true });
+    document.addEventListener('webkitfullscreenchange', () => {
+      const fs = document.fullscreenElement || document.webkitFullscreenElement;
+      if (fs) {
+        try {
+          screen.orientation.lock('landscape').catch(() => {});
+        } catch (e) {}
+      }
+    }, { once: true });
+    if (!this.scale.isFullscreen) {
+      this.scale.startFullscreen();
+    }
   }
   _createCloseButton() {
     const { width } = this.scale;
-    const r = 22;
-    const margin = 20;
+    this._btnR = 22;
+    this._btnMargin = 20;
     this._btnBg = this.add.graphics().setDepth(10);
     this._btnLabel = this.add.text(0, 0, '✕', {
       fontSize: '20px',
@@ -34,9 +42,13 @@ class StartScene extends Phaser.Scene {
     }).setOrigin(0.5).setDepth(11).setInteractive({ useHandCursor: true });
     this._btnLabel.on('pointerover', () => this._drawBtn(true));
     this._btnLabel.on('pointerout',  () => this._drawBtn(false));
-    this._btnLabel.on('pointerup',   () => { window.location.href = 'index.html'; });
-    this._btnR = r;
-    this._btnMargin = margin;
+    this._btnLabel.on('pointerup',   () => {
+      if (document.fullscreenElement) {
+        document.exitFullscreen().finally(() => { window.location.href = 'index.html'; });
+      } else {
+        window.location.href = 'index.html';
+      }
+    });
     this._repositionCloseButton(width);
   }
   _repositionCloseButton(w) {
